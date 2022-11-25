dbd0f9dc814c6035375e1a5a2000f6c8433a3c9d
Murilo Ferreira <murilosantos.ms791@gmail.com>
2022-11-25 01:34:21 -0300

    Start  Project

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-app class="app">
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
+</template>
+<style scoped>
+.app {
+  background-color: grey;
+}
+</style>
+<script>
+import "./css/index.css";
+export default {
+  name: "App",
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-card class="mx-auto">
+    <div class="img">
+      <v-avatar color="primary" size="200"></v-avatar>
+    </div>
+    <v-list nav dense class="mx-auto">
+      <v-list-item-group v-model="selectedItem" color="gray--light">
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          :to="item.to"
+          link
+          class="button-access"
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list-item-group>
+    </v-list>
+  </v-card>
+</template>
+<style scoped></style>
+<script>
+import "../css/card.css";
+export default {
+  data: () => ({
+    items: [
+      { title: "Portifolio", icon: "mdi-email", to: "/" },
+      {
+        title: "Github",
+        icon: "mdi-account-supervisor-circle",
+        to: "/",
+      },
+      { title: "Linkdin", icon: "mdi-clock-start", to: "/" },
+      { title: "Sobre", icon: "mdi-email", to: "/" },
+      { title: "Contato", icon: "mdi-email", to: "/" },
+      { title: "Instagram", icon: "mdi-email", to: "/" },
+    ],
+  }),
+};
+</script>
+<style scoped>
+.img {
+  border-radius: 50%;
+}
+</style>
